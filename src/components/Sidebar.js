e93212c49cd07c06aa064fe1094582328c9ebd03
victormarzo <victor.marzo.monteiro@gmail.com>
2022-07-16 15:36:57 -0300

    usuario,sugestoes,stories

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,79 +1,65 @@
 export default function Sidebar(){
+  function Sugestao(props){
     return(
-        <div class="sidebar">
-          <div class="usuario">
-            <img src="assets/img/catanacomics.svg" />
-            <div class="texto">
-              <strong>catanacomics</strong>
-              Catana
+      <div class="sugestao">
+        <div class="usuario">
+          <img src={props.img} />
+          <div class="texto">
+            <div class="nome">{props.nome}</div>
+            <div class="razao">{props.razao}</div>
             </div>
-          </div>
+        </div>
+        <div class="seguir">Seguir</div>
+      </div>
+    )
+  }  
+  const sugestoes = [
+    {img:"assets/img/bad.vibes.memes.svg",
+    nome:"bad.vibes.memes",
+    razao:"Segue você"},
+    {img:"assets/img/chibirdart.svg",
+    nome:"chibirdart",
+    razao:"Segue você"},
+    {img:"assets/img/razoesparaacreditar.svg",
+    nome:"razoesparaacreditar",
+    razao:"Novo no Instagram"},
+    {img:"assets/img/adorable_animals.svg",
+    nome:"adorable_animals",
+    razao:"Segue você"},
+    {img:"assets/img/smallcutecats.svg",
+    nome:"smallcutecats",
+    razao:"Segue você"}
+  ];
+  function Usuario(props){
+    return(
+      <div class="usuario">
+            <img src={props.img} />
+            <div class="texto">
+              <strong>{props.strong}</strong>
+              {props.nome}
+            </div>
+      </div>
+    )
+  }
+  return(
+        <div class="sidebar">
+          
+          <Usuario
+          img ="assets/img/catanacomics.svg"
+          strong ="catanacomics"
+          nome ="Catana"
+          />
 
           <div class="sugestoes">
             <div class="titulo">
               Sugestões para você
               <div>Ver tudo</div>
             </div>
-
-            <div class="sugestao">
-              <div class="usuario">
-                <img src="assets/img/bad.vibes.memes.svg" />
-                <div class="texto">
-                  <div class="nome">bad.vibes.memes</div>
-                  <div class="razao">Segue você</div>
-                </div>
-              </div>
-
-              <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-              <div class="usuario">
-                <img src="assets/img/chibirdart.svg" />
-                <div class="texto">
-                  <div class="nome">chibirdart</div>
-                  <div class="razao">Segue você</div>
-                </div>
-              </div>
-
-              <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-              <div class="usuario">
-                <img src="assets/img/razoesparaacreditar.svg" />
-                <div class="texto">
-                  <div class="nome">razoesparaacreditar</div>
-                  <div class="razao">Novo no Instagram</div>
-                </div>
-              </div>
-
-              <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-              <div class="usuario">
-                <img src="assets/img/adorable_animals.svg" />
-                <div class="texto">
-                  <div class="nome">adorable_animals</div>
-                  <div class="razao">Segue você</div>
-                </div>
-              </div>
-
-              <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-              <div class="usuario">
-                <img src="assets/img/smallcutecats.svg" />
-                <div class="texto">
-                  <div class="nome">smallcutecats</div>
-                  <div class="razao">Segue você</div>
-                </div>
-              </div>
-
-              <div class="seguir">Seguir</div>
-            </div>
+            {  sugestoes.map(sugestao=><Sugestao 
+              img={sugestao.img} 
+              nome={sugestao.nome} 
+              razao={sugestao.razao}/>)
+            }
           </div>
 
           <div class="links">
